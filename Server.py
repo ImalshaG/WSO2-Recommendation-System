c385e03ccfa191427cef61616f7a3d9977b65a28
ImalshaG <imalsha.sg@gmail.com>
2019-10-30 14:53:30 +0530

    Python flask server

--- Server.py
+++ Server.py
@@ -1,8 +1,8 @@
-from Main import *
+from Recommendation import *
 import flask
 from flask import request, jsonify
 
-API_dictionary = getAPIdetails()
+API_dictionary = get_API_dictionaries()
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -10,10 +10,10 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Recommendations for the API store</h1>
-<p>A prototype API to show the recommended APIs for a given user.\n The recommended APIs are: </p>'''
+<p>A prototype API to show the recommended APIs for a given user. </p>'''
 
 @app.route('/recommendations/<user>', methods=['GET'])
 def api_all(user):
-    recommendation = getRecommendations(user,API_dictionary)
+    recommendation = get_recommendations(user,API_dictionary)
     return jsonify({'User': user,'User Recommendations': recommendation})
 app.run()
